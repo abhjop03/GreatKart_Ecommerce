5fcd230b2253f296ccb4e9cef1e38822a2c841da
abhjop03 <abhishekyadavjop@gmail.com>
2025-06-02 21:19:09 +0530

    in cart availability Feature

--- carts/context_processors.py
+++ carts/context_processors.py
@@ -3,7 +3,13 @@ from .views import _cart_session_key
 
 
 def cart_item_count(requests):
-    cart = Cart.objects.get(cart_id=_cart_session_key(requests))
-    cart_items = CartItem.objects.filter(cart=cart, is_active=True)
-    total_cart_items = cart_items.count()
+    if 'admin' in requests.path:
+        return {}
+    else:
+        try:
+            cart = Cart.objects.get(cart_id=_cart_session_key(requests))
+            cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+            total_cart_items = cart_items.count()
+        except Cart.DoesNotExist:
+            total_cart_items = 0
     return dict(total_cart_items=total_cart_items)
--- store/views.py
+++ store/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
+from carts.models import CartItem
+from carts.views import _cart_session_key
 
 # Create your views here.
 
 
-def store(request, category_slug=None):
-    print("Category slug:", category_slug)
-    products = None
-    category = None
-    
+def store(request, category_slug=None):    
     if category_slug != None:
         category = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=category, is_available=True)
@@ -22,17 +20,17 @@ def store(request, category_slug=None):
         'products': products,
         'product_count': product_count,
     }
-    print("Products:", products)
-    print("Product count:", product_count)
     return render(request, 'store/store.html', context=context)
 
 def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_session_key(request), product=single_product).exists()
     except Exception as e:
         raise e
     context = {
         'single_product': single_product,
+        'in_cart': in_cart,
     }
     print("Category slug:", category_slug)
     print("Product slug:", product_slug)
